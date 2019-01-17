acbfc0bb5a1935e73fd5264b0fd654a956828daf
lambda-zhang <ggxxde@163.com>
2019-01-17 15:56:50 +0800

    use localhost

--- python/main.py
+++ python/main.py
@@ -51,24 +51,13 @@ def clean_dict():
     t = Timer(EXPIRED_TIME, clean_dict)
     t.start()
 
- 
-def get_host_ip():
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        s.connect(('8.8.8.8', 80))
-        ip = s.getsockname()[0]
-    finally:
-        s.close()
- 
-    return ip
-
 if __name__ == '__main__':
     os.system("echo 0 > /sys/class/leds/ledblue/brightness")
 
     #clean_dict()
 
     # ip修改为盒子的地址
-    client.connect(str(get_host_ip()), 1883, 60)
+    client.connect('127.0.0.1', 1883, 60)
 
     # Blocking call that processes network traffic, dispatches callbacks and
     # handles reconnecting.
